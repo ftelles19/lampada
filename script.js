171ccaa80c62f392c799696a5892000966cc3d83
ftelles19 <felipetgoncalves@outlook.com>
2024-04-21 19:34:01 -0300

    commit projeto-lampada

---
+++ script.js
@@ -0,0 +1,10 @@
+function ligarLampada() {
+    var lampadaImg = document.getElementById('lampada');
+    lampadaImg.src = 'lampada-ligada.jpg';
+  }
+  
+  function desligarLampada() {
+    var lampadaImg = document.getElementById('lampada');
+    lampadaImg.src = 'lampada-desligada.jpg';
+  }
+  
